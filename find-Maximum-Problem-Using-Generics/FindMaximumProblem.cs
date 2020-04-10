76a72a67e97fada18433e495992a501eb312516c
pranaliandre <pranaliandre06@gmail.com>
2020-04-10 13:04:02 -0500

    Added: Generic class to take in 3 variables of generic type for test case same

--- find-Maximum-Problem-Using-Generics/FindMaximumProblem.cs
+++ find-Maximum-Problem-Using-Generics/FindMaximumProblem.cs
@@ -3,6 +3,15 @@ namespace find_Maximum_Problem_Using_Generics
 {
     public class FindMaximumProblem<T> where T : IComparable
     {
+        readonly T number1;
+        readonly T number2;
+        readonly T number3;
+        public FindMaximumProblem(T number1,T number2,T number3)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+            this.number3 = number3;
+        }
         /// <summary>
         /// Method to find maximum number to given three integer number
         /// </summary>
@@ -10,7 +19,7 @@ namespace find_Maximum_Problem_Using_Generics
         /// <param name="number2">Second integer number</param>
         /// <param name="number3">Third integer number</param>
         /// <returns>maximum number</returns>
-        public T Maximum(T number1, T number2, T number3)
+        public T Maximum()
         {
             T maximumNo = number1;
             if (number2.CompareTo(maximumNo) > 0)
@@ -24,33 +33,33 @@ namespace find_Maximum_Problem_Using_Generics
             return maximumNo;
         }
     }
-     public class FindMaximum
-     {
+    public class FindMaximum
+    {
         /// <summary>
         /// Main method
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("!!!Welcome to find maximum number!!!");
-            FindMaximumProblem<int> findIntegerNumber = new FindMaximumProblem<int>();
-            FindMaximumProblem<float> findFloatNumber = new FindMaximumProblem<float>();
-            FindMaximumProblem<string> findStringNumber = new FindMaximumProblem<string>();
-            int integerMaximum = findIntegerNumber.Maximum(10,2,6);
-            Console.WriteLine("Maximum integer number : "+integerMaximum);
-            float floatMaximum = findFloatNumber.Maximum(1.1F, 10.5F, 6.6F);
-            Console.WriteLine("Maximum float number : "+floatMaximum);
-            string stringMaximum = findStringNumber.Maximum("Sanjivani","aryan","kimaya");
-            Console.WriteLine("Maximum string name : "+stringMaximum);
+            FindMaximumProblem<int> findIntegerNumber = new FindMaximumProblem<int>(10, 2, 6);
+            FindMaximumProblem<float> findFloatNumber = new FindMaximumProblem<float>(1.1F, 10.5F, 6.6F);
+            FindMaximumProblem<string> findStringNumber = new FindMaximumProblem<string>("Sanjivani", "aryan", "kimaya");
+            int integerMaximum = findIntegerNumber.Maximum();
+            Console.WriteLine("Maximum integer number : " + integerMaximum);
+            float floatMaximum = findFloatNumber.Maximum();
+            Console.WriteLine("Maximum float number : " + floatMaximum);
+            string stringMaximum = findStringNumber.Maximum();
+            Console.WriteLine("Maximum string name : " + stringMaximum);
         }
         /// <summary>
         /// Method to Compare Interface Implementation 
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public int CompareTo(object obj)
-        {
-            throw new NotImplementedException();
-        }
+        //public int CompareTo(object )
+       // {
+       //     throw new NotImplementedException();
+       // }
     }
 }
--- test-find-Maximum-Using-Generics/FindMaximumTest.cs
+++ test-find-Maximum-Using-Generics/FindMaximumTest.cs
@@ -11,7 +11,7 @@ namespace test_find_Maximum_Using_Generics
         /// <summary>
         /// Method to find maximum of three integer number
         /// </summary>
-       
+
         [Test]
         public void GivenThreeInteger_ShouldReturnMaximumNumber()
         {
@@ -19,8 +19,8 @@ namespace test_find_Maximum_Using_Generics
             int number1 = 2;
             int number2 = 10;
             int number3 = 6;
-            FindMaximumProblem<int> findMaximum = new FindMaximumProblem<int>();
-            int maximum = findMaximum.Maximum(number1, number2, number3); //find maximum number
+            FindMaximumProblem<int> findMaximum = new FindMaximumProblem<int>(number1, number2, number3);
+            int maximum = findMaximum.Maximum(); //find maximum number
             Assert.AreEqual(expected, maximum);
         }
         /// <summary>
@@ -33,8 +33,8 @@ namespace test_find_Maximum_Using_Generics
             float number1 = 2.5F;
             float number2 = 10.4F;
             float number3 = 6.6F;
-            FindMaximumProblem<float> findMaximum = new FindMaximumProblem<float>();
-            float maximum = findMaximum.Maximum(number1, number2, number3); //find maximum number
+            FindMaximumProblem<float> findMaximum = new FindMaximumProblem<float>(number1, number2, number3);
+            float maximum = findMaximum.Maximum(); //find maximum number
             Assert.AreEqual(expected, maximum);
         }
         [Test]
@@ -43,12 +43,12 @@ namespace test_find_Maximum_Using_Generics
         /// </summary>
         public void GivenThreeString_ShouldReturnMaximum()
         {
-            string expected ="sanjivani";
+            string expected = "sanjivani";
             string name1 = "aryan";
             string name2 = "sanjivani";
             string name3 = "kimaya";
-            FindMaximumProblem<string> findMaximum = new FindMaximumProblem<string>();
-            string maximum = findMaximum.Maximum(name1, name2, name3); //find maximum name
+            FindMaximumProblem<string> findMaximum = new FindMaximumProblem<string>(name1, name2, name3);
+            string maximum = findMaximum.Maximum(); //find maximum name
             Assert.AreEqual(expected, maximum);
         }
     }
