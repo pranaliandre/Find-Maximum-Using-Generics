ad85255a5afe3819e0f504337617b8f342287d2b
pranaliandre <pranaliandre06@gmail.com>
2020-04-10 04:18:28 -0500

    Added: Given three integer number and find maximum number test case for same

--- find-Maximum-Problem-Using-Generics/FindMaximumProblem.cs
+++ find-Maximum-Problem-Using-Generics/FindMaximumProblem.cs
@@ -3,11 +3,34 @@
 namespace find_Maximum_Problem_Using_Generics
 {
     public class FindMaximumProblem
-    { 
+    {
+        /// <summary>
+        /// Method to find maximum number to given three integer number
+        /// </summary>
+        /// <param name="number1">First integer number</param>
+        /// <param name="number2">Second integer number</param>
+        /// <param name="number3">Third integer number</param>
+        /// <returns>maximum number</returns>
+        public int IntegerMaximum(int number1, int number2, int number3)
+        {
+            int maximumNo = number1;
+            if (number2.CompareTo(maximumNo) > 0)
+            {
+                maximumNo = number2;
+            }
+            if (number3.CompareTo(maximumNo) > 0)
+            {
+                maximumNo = number3;
+            }
+            return maximumNo;
+        }
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("!!!Welcome to find maximum number!!!");
         }
-       
     }
 }
--- test-find-Maximum-Using-Generics/FindMaximumTest.cs
+++ test-find-Maximum-Using-Generics/FindMaximumTest.cs
@@ -8,11 +8,24 @@ namespace test_find_Maximum_Using_Generics
         public void Setup()
         {
         }
-
+        /// <summary>
+        /// Method to find maximum of three integer number
+        /// </summary>
+        /// <param name="number1">number1</param>
+        /// <param name="number2"> number2</param>
+        /// <param name="number3"> number3</param>
+        /// /// <return> maximum number </return>
         [Test]
-        public void Test1()
+        public void GivenThreeInteger_ShouldReturnMaximumNumber()
         {
-            
+            int expected = 10;
+            int number1 = 2;
+            int number2 = 10;
+            int number3 = 6;
+            FindMaximumProblem findMaximum = new FindMaximumProblem();
+            int maximum = findMaximum.IntegerMaximum(number1, number2, number3); //find maximum number
+            Assert.AreEqual(expected, maximum);
         }
+
     }
 }
