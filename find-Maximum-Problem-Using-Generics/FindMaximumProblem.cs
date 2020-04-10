938dab763773ce82556fe375713b4f37ce2341fd
pranaliandre <pranaliandre06@gmail.com>
2020-04-10 03:49:31 -0500

    Added: Create project find maximum welcome to find maximum problem

---
+++ find-Maximum-Problem-Using-Generics/FindMaximumProblem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace find_Maximum_Problem_Using_Generics
+{
+    public class FindMaximumProblem
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!!Welcome to find maximum number!!!");
+        }
+       
+    }
+}
---
+++ test-find-Maximum-Using-Generics/FindMaximumTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using find_Maximum_Problem_Using_Generics;
+namespace test_find_Maximum_Using_Generics
+{
+    public class FindMaximumTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
